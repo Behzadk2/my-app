7ac46a05c58ba403f17c95bb4df093238a585d55
behzad kazemi <bkazemi1994@gmail.com>
2023-07-29 18:11:26 +0200

    Create Header

---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header(){
+
+
+
+    return (
+
+        <>
+        <h1>Click on Dice to offer a random movie</h1>
+        </>
+    );
+
+
+}
+export default Header;
