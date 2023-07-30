80f83a2c09659b115b99eade46b80bd02807bf11
behzad kazemi <bkazemi1994@gmail.com>
2023-07-30 14:22:54 +0200

    add Dice component

--- src/App.js
+++ src/App.js
@@ -4,21 +4,30 @@ import axios from "axios";
 import Header from "./Components/Header";
 import { useEffect, useState } from "react";
 import MovieContent from "./Components/MovieContent";
+import Dice from "./Components/Dice";
 
 
 
 function App() {
 const[imovie,setImovie] = useState('')
 
+function fc(){
+  axios.get("https://raw.githubusercontent.com/Behzadk2/imdb-top-250/main/List.json").then((res) =>
+  setImovie(res.data.movies[Math.floor(Math.random()*250)]) 
+  )
+}
+
+
   useEffect(() => {
     axios.get("https://raw.githubusercontent.com/Behzadk2/imdb-top-250/main/List.json").then((res) =>
-     setImovie(res.data.movies[Math.floor(Math.random()*250)]) 
+     setImovie(res.data.movies[0]) 
      )
   },[])
   return (
     <>
     <Header></Header>
     <MovieContent moviename={imovie.title} movieRank={imovie.rank}></MovieContent>
+    <Dice onclick={fc}></Dice>
     </>
   );
 }
---
+++ src/Components/Dice.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+function Dice(props){
+
+   
+    return(
+    <>
+    
+    <button onClick={props.onclick} > click on me </button>
+    
+    
+    </>
+    )
+}
+export default Dice;
